7ced809fb104f803b70ecc2a879d37e33703f131
hyungeunjo8 <hyungeunjo@weversecompany.com>
2022-08-12 19:12:12 +0900

    feat: db

--- src/main/java/com/weverse/secretsmanagerpractice/SecretsManagerPracticeApplication.java
+++ src/main/java/com/weverse/secretsmanagerpractice/SecretsManagerPracticeApplication.java
@@ -15,10 +15,22 @@ public class SecretsManagerPracticeApplication {
 	@Value("${key2}")
 	private String h2Password;
 
+	@Value("${db.url}")
+	private String url;
+
+	@Value("${db.user}")
+	private String user;
+
+	@Value("${db.password}")
+	private String password;
+
 	@PostConstruct
 	public void checkSecretManagerProperties() {
 		System.out.println(h2User);
 		System.out.println(h2Password);
+		System.out.println(url);
+		System.out.println(user);
+		System.out.println(password);
 	}
 
 	public static void main(String[] args) {
